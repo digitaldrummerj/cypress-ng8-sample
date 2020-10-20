8e5f4471fa9454cfe1f74666a0a1eea7c7822674
Justin James <justin.james@intel.com>
2020-10-20 14:58:43 -0700

    update: tests to try to work around debounce time issues but tests still fail

--- cypress/integration/single-test.js
+++ cypress/integration/single-test.js
@@ -3,11 +3,10 @@
 describe('single test', () => {
   describe('using clock/tick statement', () => {
     before(() => {
-      cy.visit('/');
-      cy.location('pathname').should('eq', '/');
     });
     beforeEach(() => {
-      // cy.clock();
+      cy.visit('/');
+      cy.location('pathname').should('eq', '/');
       cy.get('[data-cy="search"]').clear();
     });
 
@@ -18,23 +17,25 @@ describe('single test', () => {
         "name": "Book 1",
         "category": "cat123"
       }).as('books');
-      cy.get('[data-cy="search"]').type('101').wait('@books', { requestTimeout: 15000});
+      cy.get('[data-cy="search"]').type('101').wait('@books', { requestTimeout: 15000 });
       cy.get('[data-cy="display"]').should('contain', 'Id: 101, Name: Book 1, Category: cat123');
     });
 
-    it('search book 102 and clock-tick', () => {
+    it('With Clock/Tick: search book 102 and clock-tick', () => {
+      cy.clock();
       cy.server();
       cy.route("http://localhost:3000/books/*", {
         "id": 102,
         "name": "Book 2",
         "category": "cat123"
       }).as('books');
-      cy.get('[data-cy="search"]').type('102').wait('@books', { requestTimeout: 15000 });
+      cy.get('[data-cy="search"]').type('102').tick(10000).wait('@books', { requestTimeout: 15000 });
       cy.get('[data-cy="display"]').should('contain', '102, Name: Book 2, Category: cat123');
 
     });
 
-    it('search book 103', () => {
+    it('With Click/Then Tick: search book 103', () => {
+      cy.clock();
       cy.server();
       cy.route("http://localhost:3000/books/*", {
         "id": 103,
@@ -42,9 +43,12 @@ describe('single test', () => {
         "category": "cat321"
       }).as('books');
       cy.get('[data-cy="search"]').type('103');
-      // cy.tick(2000);
-      cy.wait('@books', { requestTimeout: 15000 });
-      cy.get('[data-cy="display"]').should('contain', '103, Name: Book 2, Category: cat321');
+      cy.clock().then((clock) => {
+        clock.tick(10000)
+        cy.wait('@books', { requestTimeout: 15000 });
+        cy.get('[data-cy="display"]').should('contain', '103, Name: Book 2, Category: cat321');
+        clock.restore();
+      })
     });
   })
 })
